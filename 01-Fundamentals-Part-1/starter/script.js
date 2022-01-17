c6bb870993a9734a4e63dcd50b7056e6c01f461b
poornachary1998 <poornachary1@gmail.com>
2022-01-17 22:57:26 +0530

    assignment -1 values and variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,29 @@
+/*
 let x = "Java Script is a High level programming language";
 if (x==="Java Script is a High level programming language") 
 alert("JS is programming language");
 console.log(10+90+3-23);
+*/
+
+ //variable is box whichwe can store a value
+
+ let myFirstJob = "Programmer";
+ let myCurrentJob = "Tester";
+
+
+ let PI = 3.14;
+
+ let $function = "we can use $ or _ infront of variable";
+
+ /*assignment
+ 1. Declare variables called 'country', 'continent' and 'population' and 
+assign their values according to your own country (population in millions)
+2. Log their values to the console
+ */
+
+let country = 'India';
+let continent = 'Asia';
+let population = '1378.6 (Millions)'
+console.log(country);
+console.log(continent);
+console.log(population);
