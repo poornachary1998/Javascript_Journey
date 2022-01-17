1cb6de9b74779727640a27ff95117346da185e7c
poornachary1998 <poornachary1@gmail.com>
2022-01-17 23:24:51 +0530

    assignment -2 done

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -3,7 +3,7 @@ let x = "Java Script is a High level programming language";
 if (x==="Java Script is a High level programming language") 
 alert("JS is programming language");
 console.log(10+90+3-23);
-*/
+
 
  //variable is box whichwe can store a value
 
@@ -18,8 +18,8 @@ console.log(10+90+3-23);
  /*assignment
  1. Declare variables called 'country', 'continent' and 'population' and 
 assign their values according to your own country (population in millions)
-2. Log their values to the console
- */
+2. Log their values to the console*/
+ 
 
 let country = 'India';
 let continent = 'Asia';
@@ -27,3 +27,51 @@ let population = '1378.6 (Millions)'
 console.log(country);
 console.log(continent);
 console.log(population);
+
+
+//boolean
+true;
+console.log(true);
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+//typeof 
+
+console.log(typeof 'number');
+console.log(typeof 23);
+console.log(typeof false);
+
+
+// dynamic typing example
+let myName = 'poorna';
+console.log(typeof myName);
+
+myName = 23;
+console.log(typeof myName);
+
+// undefined example
+
+let myLastName;
+console.log(myLastName);
+console.log(typeof myLastName);
+
+myLastName = 'Narsingosu';
+console.log(typeof myLastName);
+
+// null example
+console.log(typeof null);
+
+// Assignment 2
+/*1. Declare a variable called 'isIsland' and set its value according to your 
+country. The variable should hold a Boolean value. Also declare a variable
+'language', but don't assign it any value yet
+2. Log the types of 'isIsland', 'population', 'country' and 'language'
+to the console*/
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
