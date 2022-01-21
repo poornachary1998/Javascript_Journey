d091653e4883896a47c3b4476ae2f31ff96f1d8d
poornachary1998 <poornachary1@gmail.com>
2022-01-21 23:45:50 +0530

    functions basics

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -11,3 +11,30 @@ if(moneyInCard)
 }
 
 let interface = true;*/
+
+//functions
+function ready(){
+    console.log(`get ready for party`);
+}
+ready();
+ready();
+
+
+//Passing values to the function
+function fruitProcessor(Apples, Oranges){
+   // console.log(Apples, Oranges);
+    const juice = `The juice is made of ${Apples} Apples and ${Oranges} Oranges`;
+    return juice;
+}
+
+//fruitProcessor(10,0); >> output will be only 10 0. The string is exceuted but doesnot show in output.
+//The result of calling function fruitProcessor(10,0 ) will be the juice value which was returned.
+
+// we need to save / capture the value.
+
+const appleJuice = fruitProcessor(10,0);
+console.log(appleJuice);
+
+//re-use function with different values
+const appleOrangeJuice = fruitProcessor(1,4);
+console.log(appleOrangeJuice);
