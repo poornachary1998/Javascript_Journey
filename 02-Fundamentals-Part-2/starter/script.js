dd94f571f764ef3740727f87757c795becd02816
poornachary1998 <poornachary1@gmail.com>
2022-01-21 23:04:10 +0530

    strict mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,13 @@
+'use strict';
+/*
+let moneyInCard = false;
+const buyCar = true;
+if(buyCar){
+moneyInCard = true;
+}
+if(moneyInCard)
+{
+    console.log('Buy Stocks');
+}
+
+let interface = true;*/
