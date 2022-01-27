39c300c16bc442fab1061f86f6262b4dc7497c66
poornachary1998 <poornachary1@gmail.com>
2022-01-27 14:42:06 +0530

    project 2 - complete
    added escape event handler

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -68,7 +68,11 @@ document.addEventListener('keydown', function (event) {
   console.log(event.key);
   //This prints which key exactly we pressed
 
-  if (event.key === 'Escape') {
-    console.log('Esc was pressed');
+  if (event.key === 'Escape' && modal.classList.contains('hidden')) 
+    // console.log('Esc was pressed');
+  // if(!modal.classList.contains('hidden'))
+  {
+closeModal();
   }
+
 });
