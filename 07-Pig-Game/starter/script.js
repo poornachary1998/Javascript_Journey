59adea5dfb3353192eab0c242e4d656775d61810
poornachary1998 <poornachary1@gmail.com>
2022-01-27 17:31:08 +0530

    Project 3 -Pig Game
    declaring variables in JS

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,9 @@
 'use strict';
+const diceEl =  document.querySelector('.dice');
+const score0El  = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+//both are same;selecting the elements
+score0El.textContent=0;
+score1El.textContent=0;
+//hide the dice
+diceEl.classList.add('hidden');
