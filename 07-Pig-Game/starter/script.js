cce6f6ea4a453f08e6a0770103a528450623789b
poornachary1998 <poornachary1@gmail.com>
2022-01-27 21:09:35 +0530

    updated: reset button and made code DRY.
    Final Update

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -11,14 +11,30 @@ const currentScore1El = document.getElementById('current--1');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
-const scores = [0,0];
-let currentScore = 0;
-let activePlayer = 0;
+
+let scores, currentScore,activePlayer,playing;
+
+const init = function(){
+    //starting condition
+ scores = [0,0];
+ currentScore = 0;
+ activePlayer = 0;
+ playing  = true;
+//new game conditions
 score0El.textContent=0;
 score1El.textContent=0;
-let playing  = true;
+currentScore0El.textContent = 0;
+currentScore1El.textContent = 0;
 //hide the dice before starting the dice
 diceEl.classList.add('hidden');
+//reset player
+player0.classList.remove('player--winner');
+player1.classList.remove('player--winner');
+player0.classList.add('player--active');
+player1.classList.remove('player--active');
+
+}
+init();
 
 const switchPlayer = function(){
     document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -70,7 +86,7 @@ document.getElementById(`score--${activePlayer}`).textContent = scores[activePla
 //2.check if player's score is >= 100
 //FInish the game
 
-if (scores[activePlayer]>=10){
+if (scores[activePlayer]>=50){
     playing = false;
 
     // if active player is 1 and he wins the game, player--1 classlist adds properties mentioned in css .player-winner
@@ -88,3 +104,4 @@ switchPlayer();
 }
 }
 });
+btnNew.addEventListener('click', init);
