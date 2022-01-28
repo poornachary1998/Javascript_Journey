9e0b8312e283e764ea671a58cc791ce9fcfe17b0
poornachary1998 <poornachary1@gmail.com>
2022-01-28 19:38:40 +0530

    hoisting and temperal dead zone

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -38,3 +38,56 @@ function calcAge(year) {
 //global variable
 const firstName = 'poorna';
 calcAge(1998);
+
+//Hoisting
+
+//samples for variables
+// console.log(me); //undefined
+// console.log(job); //error:Cannot access 'job' before initialization i.e., in TDz
+// console.log(year); //Cannot access 'year' before initialization i.e., in TDz
+//let us see what happens if we use them before declerations
+
+var me = 'jonas';
+let job = 'teacher';
+const year = 1991;
+
+//samples for functions
+
+//calling function declearation before intialization
+const final = addi(10, 5);
+console.log(final); // fnction decleration : this one works and gives the out put as 15
+
+//calling function expression before initialozation
+// const fina = total(10, 6);
+// console.log(fina); //function expression, gives an error Cannot access 'total' before initialization
+
+//calling arrow function before initialization
+// mul(1, 5); //gives the error cannot access 'mul' before initialization
+
+//function declearation
+function addi(a, b) {
+  return a + b;
+}
+//function expression
+const total = function (a, b) {
+  // as this starts with const it is in temp dead zone
+  return a - b;
+};
+
+//arrow functions:
+const mul = (a, b) => a * b;
+
+//fictional function example
+console.log(undefined);
+//it shows the output in console
+if (!products) deleteAllProducts();
+//In above line we are using products which is a var type so output will be undefined but this still works
+// because if(not undefined) then it deletes all products
+//hence use const most of time
+var products = 10;
+
+function deleteAllProducts() {
+  console.log('delete all products');
+}
+//declare varaiables in top of code. looks better
+// always declare the functions and then use them only after declearations
