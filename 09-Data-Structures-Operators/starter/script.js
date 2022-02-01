a56f7c9d7edbbc70938de821257c891a468ee347
poornachary1998 <poornachary1@gmail.com>
2022-02-01 22:15:17 +0530

    Update script.js

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -522,34 +522,9 @@ console.log(entries);
 // [[Prototype]]: Array(0)
 
 for (const [key, { open, close }] of entries) {
-  console.log(`On {$day} we open at ${open} and close at ${close}`);
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
 }
 
 // On {$day} we open at 12 and close at 22
 // script.js:525 On {$day} we open at 11 and close at 23
 // script.js:525 On {$day} we open at 0 and close at 24
-
-/*
-Let's continue with our football betting app! Keep using the 'game' variable from 
-before.
-Your tasks:
-1. Loop over the game.scored array and print each player name to the console, 
-along with the goal number (Example: "Goal 1: Lewandowski")
-2. Use a loop to calculate the average odd and log it to the console (We already 
-studied how to calculate averages, you can go check if you don't remember)
-3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
-Odd of victory Bayern Munich: 1.33
-Odd of draw: 3.25
-Odd of victory Borrussia Dortmund: 6.5
-Get the team names directly from the game object, don't hardcode them 
-(except for "draw"). Hint: Note how the odds and the game objects have the 
-same property names �
-4. Bonus: Create an object called 'scorers' which contains the names of the 
-players who scored as properties, and the number of goals as the value. In this 
-game, it will look like this:
-{
- Gnarby: 1,
- Hummels: 1,
- Lewandowski: 2
-}
-*/
