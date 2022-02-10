2f384811a387c9974f3e97d2a6e899e5765c3a93
poornachary1998 <poornachary1@gmail.com>
2022-02-10 23:01:12 +0530

    immediately invoked function expression

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -274,3 +274,27 @@ const oper = function (rate) {
 
 const operInput = oper(0.23);
 console.log(operInput(100));
+
+// Immediately invokde function expression IIFE
+
+(function () {
+  console.log(`this will run only once`);
+})();
+
+(() => console.log(`This  will never run`))();
+/*
+This is more of a thinking challenge than a coding challenge �
+Your tasks:
+
+1. Take the IIFE below and at the end of the function, attach an event listener that 
+changes the color of the selected h1 element ('header') to blue, each time 
+the body element is clicked. Do not select the h1 element again!
+
+2. And now explain to yourself (or someone around you) why this worked! Take all 
+the time you need. Think about when exactly the callback function is executed, 
+and what that means for the variables involved in this example.
+
+(function () {
+ const header = document.querySelector('h1');
+ header.style.color = 'red';
+*/
