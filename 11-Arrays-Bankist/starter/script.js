d50b801113f628ab2f6343fe81af9ae878d63694
poornachary1998 <poornachary1@gmail.com>
2022-03-03 00:04:01 +0530

    filter and reduce methods in project

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -83,6 +83,12 @@ containerMovements.insertAdjacentHTML('afterbegin', html)
 };
 dispalyMovements(account1.movements);
 
+const displayLabelBalance  =  function(movements){
+  const balance = movements.reduce((acc,mov)=>acc+mov,0);
+  labelBalance.textContent = `${balance} EUR`
+}
+displayLabelBalance(account1.movements);
+
 const user = 'Poorna Chary Narsingosu'; //pcn
 
 const createUserName = function (accs){
@@ -92,201 +98,6 @@ const createUserName = function (accs){
 };
 createUserName(accounts);
 console.log(accounts);
-
-
-// map(function (name)
-// {return name[0]}).join('');
-
-// console.log(username);
-//after using split ['poorna', 'chary', 'narsingosu']
-// map(function (name){return name[0]});['p', 'c', 'n']
-// .join('') pcn;
-
-
-
-
-
-
-
-// console.log(containerMovements.innerHTML);
-
-/*
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-const movementsDescription = movements.map
-((mov, i) => `Movement ${i+1}: You ${mov >0 ? 'deposited' : 'withdrew'} ${Math.abs(mov)}`);
-
-console.log(movementsDescription);
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-// map method
-/*
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-const euroToUsd = 1.1;
-
-// const movementsUSD = movements.map(function(mov){
-//   return mov * euroToUsd;
-// });
-
-const movementsUSD = movements.map(mov => mov * euroToUsd);
-console.log(movements);
-console.log(movementsUSD);
-
-// same above example with for of 
-const newArr = [];
-
-for(const mov of movements) newArr.push(mov * euroToUsd);
-console.log(newArr 
-);
-*/
-
-
-
-
-
-
-
-
-
-/**
- * 
- * Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners 
-about their dog's age, and stored the data into an array (one array for each). For 
-now, they are just interested in knowing whether a dog is an adult or a puppy.
-A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years 
-old.
-Your tasks:
-Create a function 'checkDogs', which accepts 2 arrays of dog's ages 
-('dogsJulia' and 'dogsKate'), and does the following things:
-1. Julia found out that the owners of the first and the last two dogs actually have 
-cats, not dogs! So create a shallow copy of Julia's array, and remove the cat 
-ages from that copied array (because it's a bad practice to mutate function 
-parameters)
-2. Create an array with both Julia's (corrected) and Kate's data
-3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 
-is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 
-�
-")
-4. Run the function for both test datasets
-Test data:
-§ Data 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
-§ Data 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
-Hints: Use tools from all lectures in this section so far 
- */
-/*
-const checkDogs = function (dogsJulia, dogsKate){
-const juliaCorrected = dogsJulia.slice();
-// console.log(juliaCorrected);
-juliaCorrected.splice(0,1);
-juliaCorrected.splice(-2);
-
-const dogs = juliaCorrected.concat(dogsKate)
-console.log(dogs);
-//  [5, 2, 4, 1, 15, 8, 3]
-dogs.forEach(function(dog,i){
-  if (dog >=3){
-    console.log(`it's an adult "Dog number ${i+1} is an adult, and is ${dog} years old"`);
-  }
-  else
-  {
-    console.log(`Dog number ${i+1} is still a puppy`);
-  }
-})
-
-}
-checkDogs([3, 5, 2, 12, 7],[4, 1, 15, 8, 3]);
-console.log(`-----------------------`);
-checkDogs([9, 16, 6, 8, 3],[10, 5, 6, 1, 4]);
-
-
-
-
-
-
-
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 /*
@@ -378,31 +189,6 @@ uniqueSet.forEach(function(value, key, map){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*
 /////////////////////////////////////////////////
 let arr = ['a', 'e', 'i', 'o', 'u'];
@@ -487,3 +273,136 @@ console.log(arr3.at(-1));
 
 console.log('jonas'.at(0));
 */
+
+
+
+/**
+ * 
+ * Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners 
+about their dog's age, and stored the data into an array (one array for each). For 
+now, they are just interested in knowing whether a dog is an adult or a puppy.
+A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years 
+old.
+Your tasks:
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages 
+('dogsJulia' and 'dogsKate'), and does the following things:
+1. Julia found out that the owners of the first and the last two dogs actually have 
+cats, not dogs! So create a shallow copy of Julia's array, and remove the cat 
+ages from that copied array (because it's a bad practice to mutate function 
+parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 
+is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 
+�
+")
+4. Run the function for both test datasets
+Test data:
+§ Data 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+§ Data 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+Hints: Use tools from all lectures in this section so far 
+ */
+/*
+const checkDogs = function (dogsJulia, dogsKate){
+const juliaCorrected = dogsJulia.slice();
+// console.log(juliaCorrected);
+juliaCorrected.splice(0,1);
+juliaCorrected.splice(-2);
+
+const dogs = juliaCorrected.concat(dogsKate)
+console.log(dogs);
+//  [5, 2, 4, 1, 15, 8, 3]
+dogs.forEach(function(dog,i){
+  if (dog >=3){
+    console.log(`it's an adult "Dog number ${i+1} is an adult, and is ${dog} years old"`);
+  }
+  else
+  {
+    console.log(`Dog number ${i+1} is still a puppy`);
+  }
+})
+
+}
+checkDogs([3, 5, 2, 12, 7],[4, 1, 15, 8, 3]);
+console.log(`-----------------------`);
+checkDogs([9, 16, 6, 8, 3],[10, 5, 6, 1, 4]);
+
+
+
+
+
+
+
+
+*/
+
+// map(function (name)
+// {return name[0]}).join('');
+
+// console.log(username);
+//after using split ['poorna', 'chary', 'narsingosu']
+// map(function (name){return name[0]});['p', 'c', 'n']
+// .join('') pcn;
+
+
+// // filter method
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// const deposits = movements.filter(function(mov){
+// return mov>0;})
+// console.log(movements);
+// console.log(deposits);
+
+// const withdrawalsFor = movements.filter(function (mov){
+// return mov<0;
+// });
+// console.log(withdrawalsFor);
+
+// reduce method
+// accumulator is the one parameter in reduce.
+// for reduce we will have 2 arguments, the second argument will be the value of accumulator
+
+const balance = movements.reduce(function(acc,cur, i, arr){
+  return acc+cur;
+},0);
+console.log(balance);
+
+
+// console.log(containerMovements.innerHTML);
+
+/*
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const movementsDescription = movements.map
+((mov, i) => `Movement ${i+1}: You ${mov >0 ? 'deposited' : 'withdrew'} ${Math.abs(mov)}`);
+
+console.log(movementsDescription);
+*/
+
+// map method
+/*
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const euroToUsd = 1.1;
+
+// const movementsUSD = movements.map(function(mov){
+//   return mov * euroToUsd;
+// });
+
+const movementsUSD = movements.map(mov => mov * euroToUsd);
+console.log(movements);
+console.log(movementsUSD);
+
+// same above example with for of 
+const newArr = [];
+
+for(const mov of movements) newArr.push(mov * euroToUsd);
+console.log(newArr 
+);
+*/
+
+
+
+
+
+
