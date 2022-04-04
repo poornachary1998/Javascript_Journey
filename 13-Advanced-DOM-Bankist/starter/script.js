211cabdf70454b525f3bfd0cb91ad062d9f116e6
poornachary1998 <poornachary1@gmail.com>
2022-04-04 23:54:32 +0530

    implementing smooth scrolling

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -31,3 +31,10 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click',funtcion(e)){
+
+  // We need to get the coordinates
+}
